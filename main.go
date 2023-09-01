1bf4c4e52a2bd013d3c6738ed9b808aa1f6f5427
Jon Bolt <jonbolt83@gmail.com>
2023-09-01 17:15:40 +0100

    fix new relic

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/honeybadger-io/honeybadger-go"
+	newrelic "github.com/newrelic/go-agent"
 	"github.com/taact-dev/heroku-cloudwatch-drain/logger"
 	"github.com/taact-dev/heroku-cloudwatch-drain/logparser"
 	"gopkg.in/tylerb/graceful.v1"
